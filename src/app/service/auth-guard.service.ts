b58470650b171848e72b836b9d347409f363bc26
marcobusacca <marcobusacca01@gmail.com>
2024-04-12 15:39:26 +0200

    [FEAT] Style of AppHeaderComponent and AuthFormsComponents

--- src/app/service/auth-guard.service.ts
+++ src/app/service/auth-guard.service.ts
@@ -2,14 +2,15 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable, map, take } from 'rxjs';
 import { AuthService } from './auth.service';
+import { UserService } from './user.service';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuardService implements CanActivate, CanActivateChild {
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private authService: AuthService, private userService: UserService, private router: Router) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Promise<boolean | UrlTree> | Observable<boolean | UrlTree> {
-    if (route.routeConfig.path !== 'sign-up' && route.routeConfig.path !== 'login') {
+    if (route.routeConfig.path === 'user/data') {
       return this.authService.user.pipe(
         take(1),
         map(user => {
@@ -28,6 +29,21 @@ export class AuthGuardService implements CanActivate, CanActivateChild {
             return true;
           }
           return this.router.createUrlTree(['/']);
+        })
+      );
+    }
+    if (route.routeConfig.path !== 'sign-up' && route.routeConfig.path !== 'login' && route.routeConfig.path !== 'user-data') {
+      return this.authService.user.pipe(
+        take(1),
+        map(user => {
+          if (user) {
+            if (this.userService.userData !== undefined) {
+              return true;
+            } else {
+              return this.router.createUrlTree(['/']);
+            }
+          }
+          return this.router.createUrlTree(['/login']);
         })
       );
     }
